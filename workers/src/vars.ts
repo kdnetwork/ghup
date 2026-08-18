ede68781b44c7a77dc2fb7cff457108108a19287
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2026-08-18 10:02:40 +0800

    update: release list cache max age 1h->30mins

--- workers/src/vars.ts
+++ workers/src/vars.ts
@@ -1,6 +1,6 @@
 import { RepoItem } from './utils/rule'
 
-export const ReleaseListCacheMaxAge = 60 * 60 // 1 hour
+export const ReleaseListCacheMaxAge = 60 * 30 // 30 mins
 export const ReleaseInfoCacheMaxAge = 60 * 60 * 24 * 180 // 180 days
 
 export interface apiVar {
